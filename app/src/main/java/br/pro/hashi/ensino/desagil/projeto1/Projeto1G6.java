d07eabd726a0e5c145391b04001ff2ab82bc403c
Leonardo <leonardom1808@gmail.com>
2019-04-19 17:27:16 -0300

    Pagina inicial

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Projeto1G6.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Projeto1G6.java
@@ -2,6 +2,7 @@ package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Projeto1G6 extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Projeto1G6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projeto1g6);
-    }
+        TextView input= findViewById(R.id.input);
+        TextView atalho = findViewById(R.id.atalho);
+
+
+}
 }
