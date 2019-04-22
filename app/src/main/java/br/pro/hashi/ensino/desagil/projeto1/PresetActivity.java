92f0dc36dd14b1c1d40574731d93b17cadee9f60
Leonardo <leonardom1808@gmail.com>
2019-04-22 18:28:24 -0300

    Atalho implementado, com alguns bugs

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/PresetActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/PresetActivity.java
@@ -1,8 +1,10 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PresetActivity extends AppCompatActivity {
 
@@ -14,6 +16,33 @@ public class PresetActivity extends AppCompatActivity {
         Button m2 = findViewById(R.id.m2);
         Button m3 = findViewById(R.id.m3);
         Button m4 = findViewById(R.id.m4);
+
+        m1.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputActivity.class);
+            setContentView(R.layout.activity_input);
+            TextView text = findViewById(R.id.edit);
+            text.setText("Pré Definida 1");
+
+
+        });
+        m2.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputActivity.class);
+            setContentView(R.layout.activity_input);
+            TextView text = findViewById(R.id.edit);
+            text.setText("Pré Definida 2");
+    });
+        m3.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputActivity.class);
+            setContentView(R.layout.activity_input);
+            TextView text = findViewById(R.id.edit);
+            text.setText("Pré Definida 3");
+        });
+        m4.setOnClickListener(view -> {
+            Intent intent = new Intent(this, InputActivity.class);
+            setContentView(R.layout.activity_input);
+            TextView text = findViewById(R.id.edit);
+            text.setText("Pré Definida 4");
+        });
     }
 
 }
