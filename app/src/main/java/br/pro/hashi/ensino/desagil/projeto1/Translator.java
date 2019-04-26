f5c575ef4a6359d9357f2eec58e9a49018a252fe
Leonardo <leonardom1808@gmail.com>
2019-04-25 22:06:29 -0300

    Tree do translator feita

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Translator.java
@@ -14,110 +14,200 @@ public class Translator {
     // Você deve mudar o recheio deste construtor,
     // de acordo com os requisitos não-funcionais.
     public Translator() {
-        root = null;
-        map = null;
+        root = new Node ('^');
+        map = new HashMap<>();
+
+        Node e = new Node('e');
+        e.setParent(root);
+        Node t = new Node('t');
+        t.setParent(root);
+        root.setLeft(e);
+        root.setRight(t);
 
 
+        Node i = new Node('i');
+        i.setParent(e);
+        e.setLeft(i);
+        Node a = new Node('a');
+        a.setParent(e);
+        e.setRight(a);
+
+
+        Node n = new Node('n');
+        n.setParent(e);
+        t.setLeft(n);
+        Node m = new Node('m');
+        m.setParent(e);
+        t.setRight(m);
+
+
+        Node s = new Node('s');
+        s.setParent(i);
+        i.setLeft(s);
+        Node u = new Node('u');
+        u.setParent(i);
+        i.setRight(u);
+
+
+        Node r = new Node('r');
+        r.setParent(a);
+        a.setLeft(r);
+        Node w = new Node('w');
+        w.setParent(a);
+        a.setRight(w);
+
+
+        Node d = new Node('d');
+        d.setParent(n);
+        n.setLeft(d);
+        Node k = new Node('k');
+        k.setParent(n);
+        n.setRight(k);
+
+
+        Node g = new Node('g');
+        g.setParent(m);
+        m.setLeft(g);
+        Node o = new Node('o');
+        o.setParent(m);
+        m.setRight(o);
+
+
+        Node h = new Node('h');
+        h.setParent(s);
+        s.setLeft(h);
+        Node v = new Node('v');
+        v.setParent(s);
+        s.setRight(v);
+
+
+        Node f = new Node('f');
+        f.setParent(u);
+        u.setLeft(f);
+        Node blank1 = new Node(' ');
+        blank1.setParent(u);
+        u.setRight(blank1);
+
+
+        Node l = new Node('l');
+        l.setParent(r);
+        r.setLeft(l);
+        Node blank2 = new Node(' ');
+        blank2.setParent(r);
+        r.setRight(blank2);
+
+
+        Node p = new Node('p');
+        p.setParent(w);
+        w.setLeft(p);
+        Node j = new Node('j');
+        j.setParent(w);
+        w.setRight(j);
+
+
+        Node b = new Node('b');
+        b.setParent(d);
+        d.setLeft(b);
+        Node x = new Node('x');
+        x.setParent(d);
+        d.setRight(x);
+
+
+        Node c = new Node('c');
+        c.setParent(k);
+        k.setLeft(c);
+        Node y = new Node('y');
+        y.setParent(k);
+        k.setRight(y);
+
+
+        Node z = new Node('z');
+        z.setParent(g)  ;
+        g.setLeft(z);
+        Node q = new Node('q');
+        q.setParent(g);
+        g.setRight(q);
+
+
+        Node blank3 = new Node(' ');
+        blank3.setParent(o);
+        o.setLeft(blank3);
+        Node blank4 = new Node(' ');
+        blank4.setParent(o);
+        o.setRight(blank4);
+
+
+        Node n5 = new Node('5');
+        n5.setParent(h);
+        h.setLeft(n5);
+        Node n4 = new Node('4');
+        n4.setParent(h);
+        h.setRight(n4);
+
+
+        Node n3 = new Node('3');
+        n3.setParent(v);
+        v.setRight(n3);
+
+
+        Node n2 = new Node('2');
+        n2.setParent(blank1);
+        blank1.setRight(n2);
+
+
+        Node plus = new Node('+');
+        plus.setParent(blank2)  ;
+        blank2.setLeft(plus);
+
+
+        Node n1 = new Node('1');
+        n1.setParent(j);
+        j.setRight(n1);
+
+
+        Node n6 = new Node('6');
+        n6.setParent(b);
+        b.setLeft(n6);
+        Node equal = new Node('=');
+        equal.setParent(b);
+        b.setRight(equal);
+
+        Node slash = new Node('/');
+        slash.setParent(x);
+        x.setLeft(slash);
+
+
+        Node n7 = new Node('7');
+        n7.setParent(z);
+        z.setLeft(n7);
+
+
+        Node n8 = new Node('8');
+        n8.setParent(blank3);
+        blank3.setLeft(n8);
+
+        Node n9 = new Node('9');
+        n9.setParent(blank4);
+        blank4.setLeft(n9);
+        Node zero = new Node('0');
+        zero.setParent(blank4);
+        blank4.setRight(zero);
+
+        
     }
 
 
     // Você deve mudar o recheio deste método, de
     // acordo com os requisitos não-funcionais.
     public char morseToChar(String code) {
-        HashMap<String,Character> dict = new HashMap<String, Character>();
-        //http://ascii-table.com/morse-code.php
-        dict.put("-----", '0');
-        dict.put(".----", '1');
-        dict.put("..---", '2');
-        dict.put("...--", '3');
-        dict.put("....-", '4');
-        dict.put(".....", '5');
-        dict.put("-....", '6');
-        dict.put("--...", '7');
-        dict.put("---..", '8');
-        dict.put("----.", '9');
-        dict.put(".-",'a');
-        dict.put("-...",'b');
-        dict.put("-.-.",'c');
-        dict.put("-..",'d');
-        dict.put(".",'e');
-        dict.put("..-.",'f');
-        dict.put("--.",'g');
-        dict.put("....",'h');
-        dict.put("..",'i');
-        dict.put(".---",'j');
-        dict.put("-.-",'k');
-        dict.put(".-..",'l');
-        dict.put("--",'m');
-        dict.put("-.",'n');
-        dict.put("---",'o');
-        dict.put(".--.",'p');
-        dict.put("--.-",'q');
-        dict.put(".-.",'r');
-        dict.put("...",'s');
-        dict.put("-",'t');
-        dict.put("..-",'u');
-        dict.put("...-",'v');
-        dict.put(".--",'w');
-        dict.put("-..-",'x');
-        dict.put("-.--",'y');
-        dict.put("--..",'z');
-
-        char c;
-
-        c = dict.get(code);
-
-        return c ;
+        return ' ';
     }
 
 
     // Você deve mudar o recheio deste método, de
     // acordo com os requisitos não-funcionais.
     public String charToMorse(char c) {
-        HashMap<Character,String> dict = new HashMap<Character, String>();
-        //http://ascii-table.com/morse-code.php
-        dict.put('0',"-----");
-        dict.put('1',".----");
-        dict.put('2',"..---");
-        dict.put('3',"...--");
-        dict.put('4',"....-");
-        dict.put('5',".....");
-        dict.put('6',"-....");
-        dict.put('7',"--...");
-        dict.put('8',"---..");
-        dict.put('9',"----.");
-        dict.put('a',".-");
-        dict.put('b',"-...");
-        dict.put('c',"-.-.");
-        dict.put('d',"-..");
-        dict.put('e',".");
-        dict.put('f',"..-.");
-        dict.put('g',"--.");
-        dict.put('h',"....");
-        dict.put('i',"..");
-        dict.put('j',".---");
-        dict.put('k',"-.-");
-        dict.put('l',".-..");
-        dict.put('m',"--");
-        dict.put('n',"-.");
-        dict.put('o',"---");
-        dict.put('p',".--.");
-        dict.put('q',"--.-");
-        dict.put('r',".-.");
-        dict.put('s',"...");
-        dict.put('t',"-");
-        dict.put('u',"..-");
-        dict.put('v',"...-");
-        dict.put('w',".--");
-        dict.put('x',"-..-");
-        dict.put('y',"-.--");
-        dict.put('z',"--..");
-
-        String str;
-
-        str = dict.get(c);
-
-        return str;
+        return " ";
     }
 
 
