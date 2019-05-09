c47d2e4f7d42b8c9e10a164a33aea8f0c5c90a05
Leonardo <leonardom1808@gmail.com>
2019-05-09 17:20:10 -0300

    Final

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/PresetActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/PresetActivity.java
@@ -1,8 +1,12 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -35,6 +39,8 @@ public class PresetActivity extends AppCompatActivity {
 
     private  ArrayList<String> arrayList = new ArrayList<>();
     private  ArrayAdapter<String> adapter;
+
+    private static final int REQUEST_SEND_SMS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,9 +93,24 @@ public class PresetActivity extends AppCompatActivity {
 
 
         send_btn.setOnClickListener(view -> {
-            Intent intent = new Intent(PresetActivity.this, ContactsActivity.class);
-            intent.putExtra("position",arrayList.get(selIndex));
-            startActivity(intent);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+
+                Intent intent = new Intent(this, ContactsActivity.class);
+                intent.putExtra("position", arrayList.get(selIndex));
+                startActivity(intent);
+            } else {
+
+
+                String[] permissions = new String[]{
+                        Manifest.permission.SEND_SMS,
+                };
+
+                ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
+            }
+
+
+
         });
 
         database.addChildEventListener(new ChildEventListener() {
@@ -124,5 +145,16 @@ public class PresetActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+
+        if (requestCode == REQUEST_SEND_SMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+            Intent intent = new Intent(this, ContactsActivity.class);
+            startActivity(intent);
+        }
     }
 }
