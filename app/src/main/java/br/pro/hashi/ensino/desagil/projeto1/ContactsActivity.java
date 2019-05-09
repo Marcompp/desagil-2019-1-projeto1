a32e9434178636ce8ec74bc3693351d65e72eee0
APaiva1999 <deco.1999@hotmail.com>
2019-05-09 17:39:39 -0300

    integrando

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
@@ -1,20 +1,14 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.content.ContentResolver;
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -22,50 +16,28 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
 public class ContactsActivity extends AppCompatActivity {
-        String nome[] = new String[] {"André", "Marco", "Pedro", "Leonardo"};
-        String num[] = new String[] {"11999078090","11949774919","6281240350","11966357075"};
-        ListView list;
+    private ListView contactList;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+        contactList = findViewById(R.id.listView);
+    }
 
-            list = findViewById(R.id.listView);
+    private void get (View v){
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        startManagingCursor(cursor);
 
-            MyAdapter adapter = new MyAdapter(this, nome, num);
-            list.setAdapter(adapter);
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone._ID};
 
-            /*list.setOnClickListener(new AdapterView.OnClickListener(){
-                @Override
-                public void onItemClick()
+        int[] to = {android.R.id.text1,android.R.id.text2};
 
-            });*/
-        }
+        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor,from,to);
+        contactList.setAdapter(simpleCursorAdapter);
+        contactList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+    }
 
-        class MyAdapter extends ArrayAdapter<String>{
-            Context context;
-            String nomes[];
-            String numeros[];
-
-            MyAdapter(Context c, String[] nome, String[] num){
-                super(c,R.layout.row,R.id.contato, nome);
-                this.context=c;
-                this.nomes=nome;
-                this.numeros=num;
-            }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.row, parent, false);
-            TextView nomes = row.findViewById(R.id.contato);
-            TextView numeros = row.findViewById(R.id.numero);
-            nomes.setText(nome[position]);
-            numeros.setText(num[position]);
-            return row;
-        }
-
-        }
 }
