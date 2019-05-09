9d3c784d3f591a2f84009fa7f61073c17b5749dc
Leonardo <leonardom1808@gmail.com>
2019-05-09 11:50:59 -0300

    Permissão na InputActivity

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/InputActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/InputActivity.java
@@ -1,6 +1,11 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +24,10 @@ public class InputActivity extends AppCompatActivity{
     private  Button spaceButton;
     private Button deleteButton;
     private Button sendButton;
+
+
+    private static final int REQUEST_SEND_SMS = 0;
+
 
     //Criando um objeto da classe Translator para traduzir o texto em Morse
     private Translator translator;
@@ -86,9 +95,28 @@ public class InputActivity extends AppCompatActivity{
         });
 
         sendButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, PermissionActivity.class);
-            startActivity(intent);
+
+            // Verifica se o aplicativo tem a permissão desejada.
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+                // Se tem, podemos iniciar a SMSActivity direto.
+                Intent intent = new Intent(this, SMSActivity.class);
+                startActivity(intent);
+            } else {
+
+                // Senão, precisamos pedir essa permissão.
+
+                // Cria um vetor de permissões a pedir. Como queremos
+                // uma só, parece um pouco feio, mas é bem conveniente
+                // quando queremos pedir várias permissões de uma vez.
+                String[] permissions = new String[]{
+                        Manifest.permission.SEND_SMS,
+                };
+
+                ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
+            }
         });
+
 
         if (savedExtra != null)
             text.setText(savedExtra);
@@ -132,5 +160,19 @@ public class InputActivity extends AppCompatActivity{
         else {word+= translator.morseToChar(content);}
 
         text.setText(word);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        // Verifica se de fato é uma resposta ao pedido acima e se a
+        // resposta foi positiva. As respostas estão armazenadas no
+        // vetor grantResults, que pode estar vazio se o usuário
+        // escolheu simplesmente ignorar o pedido e não responder nada.
+        if (requestCode == REQUEST_SEND_SMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            // Se foi positiva, podemos iniciar a SMSActivity.
+            Intent intent = new Intent(this, SMSActivity.class);
+            startActivity(intent);
+        }
     }
 }
