b2f7a472db6623817dd6fca8f413d7dd82457478
APaiva1999 <deco.1999@hotmail.com>
2019-05-09 18:11:36 -0300

    lista contatos

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
@@ -1,6 +1,7 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -8,7 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -16,35 +18,29 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import br.pro.hashi.ensino.desagil.projeto1.R;
 
 public class ContactsActivity extends AppCompatActivity {
-    private ListView contactList;
-    private Button btn_get;
+    String nome[]= new String[] {"André"};
+    String numero[] = new String[] {"11999078090"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacts);
-        contactList = findViewById(R.id.listView);
-        btn_get = findViewById(R.id.pegaContatos);
+        setContentView(R.layout.activity_preset);
 
-        btn_get.setOnClickListener(v -> {
-            get(v);
-
+        ListView listView = findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,nome);
+        ArrayAdapter<String> adpter2 = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_2,numero);
+        listView.setAdapter(adapter);
+        listView.setAdapter(adpter2);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ContactsActivity.this,InputActivity.class);
+                intent.putExtra("position",numero[position]);
+                startActivity(intent);
+            }
         });
     }
-
-    private void get (View v){
-        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-        startManagingCursor(cursor);
-
-        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER,ContactsContract.CommonDataKinds.Phone._ID};
-
-        int[] to = {android.R.id.text1,android.R.id.text2};
-
-        SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor,from,to);
-        contactList.setAdapter(simpleCursorAdapter);
-        contactList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
-    }
-
 }
