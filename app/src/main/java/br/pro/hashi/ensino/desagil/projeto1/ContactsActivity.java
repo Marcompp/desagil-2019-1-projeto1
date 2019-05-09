a31d77b3a41731d33a75e9bbe3548a88714ef35d
APaiva1999 <deco.1999@hotmail.com>
2019-05-09 15:19:46 -0300

    att teste

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ContactsActivity.java
@@ -1,12 +1,14 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -16,7 +18,7 @@ import java.util.ArrayList;
 
 public class ContactsActivity extends AppCompatActivity {
         String nome[] = new String[] {"André", "Marco", "Pedro", "Leonardo"};
-        String num[] = new String[] {"11999078090","11","",""}
+        String num[] = new String[] {"11999078090","11949774919","6281240350","11966357075"}
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +26,16 @@ public class ContactsActivity extends AppCompatActivity {
             setContentView(R.layout.activity_preset);
 
             ListView listView = findViewById(R.id.listView);
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,msgpd);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,nome);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_2,num);
             listView.setAdapter(adapter);
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Intent intent = new Intent(PresetActivity.this,InputActivity.class);
-                    intent.putExtra("position",msgpd[position]);
+                    intent.putExtra("position",num[position]);
                     startActivity(intent);
                 }
             });
         }
     }
-}
