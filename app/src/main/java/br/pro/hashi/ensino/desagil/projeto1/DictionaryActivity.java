5de1266880c105707301a6335458258e50acc728
Marcompp <marcomoliternop@gmail.com>
2019-05-10 11:43:04 -0300

    tentativa 2

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DictionaryActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/DictionaryActivity.java
@@ -3,11 +3,12 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.Collections;
 import java.util.LinkedList;
 
@@ -15,23 +16,23 @@ public class DictionaryActivity extends AppCompatActivity {
 
     private ListView dict;
 
-    LinkedList<String> Mtl = new LinkedList<String>();
-    LinkedList<String> Ltm = new LinkedList<String>();
+    LinkedList<String> Mtl;
+    LinkedList<String> Ltm;
     char Alfabeto[] = {'a','b','c','d','e','f','g','h','i',
             'j','k','l','m','n','o','p','q','r','s','t','j',
             'v','w','x','y','z','0','1','2','3','4','5','6',
             '7','8','9','/','+','='};
-    LinkedList<String> Letras = new LinkedList<String>();
-    LinkedList<String> Letra1 = new LinkedList<String>();
-    LinkedList<String> Letra2 = new LinkedList<String>();
-    LinkedList<String> Letra3 = new LinkedList<String>();
-    LinkedList<String> Letra4 = new LinkedList<String>();
-    LinkedList<String> Letra5 = new LinkedList<String>();
+    LinkedList<String> Letras;
+    LinkedList<String> Letra1;
+    LinkedList<String> Letra2;
+    LinkedList<String> Letra3;
+    LinkedList<String> Letra4;
+    LinkedList<String> Letra5;
 
-
-    private boolean ismtv;
 
     private Translator translator;
+
+    private boolean ismtv = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +41,16 @@ public class DictionaryActivity extends AppCompatActivity {
 
         dict = findViewById(R.id.listView);
         translator = new Translator();
+
+        Mtl = new LinkedList<>();
+        Ltm = new LinkedList<>();
+
+        Letra1 = new LinkedList<String>();
+        Letras = new LinkedList<String>();
+        Letra2 = new LinkedList<String>();
+        Letra3 = new LinkedList<String>();
+        Letra4 = new LinkedList<String>();
+        Letra5 = new LinkedList<String>();
 
         Letra1.add(".");
         Letra1.add("/");
@@ -50,24 +61,28 @@ public class DictionaryActivity extends AppCompatActivity {
             Letra2.add(letra+"-");
             Letras.add(letra+".");
             Letras.add(letra+"-");
+            System.out.println(1);
         }
         for (String letra:Letra2) {
             Letra3.add(letra+".");
             Letra3.add(letra+"-");
             Letras.add(letra+".");
             Letras.add(letra+"-");
+            System.out.println(2);
         }
         for (String letra:Letra3) {
             Letra4.add(letra+".");
             Letra4.add(letra+"-");
             Letras.add(letra+".");
             Letras.add(letra+"-");
+            System.out.println(3);
         }
         for (String letra:Letra4) {
             Letra5.add(letra+".");
             Letra5.add(letra+"-");
             Letras.add(letra+".");
             Letras.add(letra+"-");
+            System.out.println(4);
         }
 
         //for (char alpha:Alfabeto) {
@@ -86,14 +101,19 @@ public class DictionaryActivity extends AppCompatActivity {
 
         Collections.sort(Ltm);
 
+        ArrayAdapter<String> mtladapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,Mtl);
+        ArrayAdapter<String> ltmadapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,Ltm);
 
 
-        ArrayAdapter<String> mtlAdapter = new ArrayAdapter<String>(this, R.layout.activity_dictionary, R.id.textView, Mtl);
-        ArrayAdapter<String> ltmAdapter = new ArrayAdapter<String>(this, R.layout.activity_dictionary, R.id.textView, Ltm);
+        //ArrayAdapter<String> mtlAdapter = new ListAdapter<>(this, R.layout.activity_dictionary, R.id.textView, Mtl);
+        //ArrayAdapter<String> ltmAdapter = new ArrayAdapter<String>(this, R.layout.activity_dictionary, R.id.textView, Ltm);
+
         if (ismtv) {
-            dict.setAdapter(mtlAdapter);
+            dict.setAdapter(ltmadapter);
         } else {
-            dict.setAdapter(mtlAdapter);
+            dict.setAdapter(mtladapter);
         }
 
 
